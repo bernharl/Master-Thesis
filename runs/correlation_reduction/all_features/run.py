98975cafa758a00b86fd8bd49ae14c530c9c435c
bernharl <bernhardnorneslotsberg@gmail.com>
2020-12-11 22:48:15 +0100

    Small update

---
+++ runs/correlation_reduction/all_features/run.py
@@ -0,0 +1,4 @@
+from camelsml import load_config, train
+
+cfg = load_config(cfg_file="run_config.txt", device="cuda:0", num_workers=24)
+print(cfg)
--- runs/correlation_reduction/setup_splits_and_normalization.py
+++ runs/correlation_reduction/setup_splits_and_normalization.py
@@ -1,5 +1,4 @@
+from camelsml import split_basins, load_config 
 
-
-from camelsml import split_basins
-
-split_basins("/home/bernhard/git/ealstm_regional_modeling_camels_gb/data/basin_list.txt", split=[0.65, 0.1, 0.25], store_folder="/home/bernhard/git/Master-Thesis/runs/correlation_reduction/basin_splits", seed=1337)
+cfg = load_config(cfg_file="camels_root_info.txt")
+split_basins(cfg["camels_root"],"/home/bernhard/git/ealstm_regional_modeling_camels_gb/data/basin_list.txt", split=[0.65, 0.1, 0.25], store_folder="/home/bernhard/git/Master-Thesis/runs/correlation_reduction/basin_splits", seed=1337)
