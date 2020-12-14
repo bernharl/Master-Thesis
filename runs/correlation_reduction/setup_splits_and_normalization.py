db14381f11c37b9def7fe540df8bcbf6702b6317
bernharl <bernhardnorneslotsberg@gmail.com>
2020-12-14 08:57:44 +0100

    Fix split, change seed

--- runs/correlation_reduction/setup_splits_and_normalization.py
+++ runs/correlation_reduction/setup_splits_and_normalization.py
@@ -1,4 +1,10 @@
-from camelsml import split_basins, load_config 
+from camelsml import split_basins, load_config
 
 cfg = load_config(cfg_file="camels_root_info.txt")
-split_basins(cfg["camels_root"],"/home/bernhard/git/ealstm_regional_modeling_camels_gb/data/basin_list.txt", split=[0.65, 0.1, 0.25], store_folder="/home/bernhard/git/Master-Thesis/runs/correlation_reduction/basin_splits", seed=1337)
+split_basins(
+    camels_root=cfg["camels_root"],
+    basin_list="/home/bernhard/git/ealstm_regional_modeling_camels_gb/data/basin_list.txt",
+    split=[0.65, 0.1, 0.25],
+    store_folder="/home/bernhard/git/Master-Thesis/runs/correlation_reduction/basin_splits",
+    seed=19970204,
+)
