527d2de0c9fa86fba20a3b02c57045586b5e0ed7
bernharl <bernhardnorneslotsberg@gmail.com>
2021-01-22 12:05:47 +0100

    Fix train_basin_file not being udated. Should not matter much

--- runs/correlation_reduction/chosen_features_cv/permutation.py
+++ runs/correlation_reduction/chosen_features_cv/permutation.py
@@ -12,7 +12,7 @@ permutation_folder.mkdir(exist_ok=True)
 cfg = load_config("run_config.txt", device="cuda:0", num_workers=24)
 np.random.seed(cfg["seed"])
 random.seed(cfg["seed"])
-for i in range(1,2):
+for i in range(0,1):
     save_path = permutation_folder / f"{i}"
     save_path.mkdir(exist_ok=True)
     cv_dir = list((Path().absolute() / f"{i}").glob("*"))
@@ -21,6 +21,7 @@ for i in range(1,2):
     else:
         cv_dir = cv_dir[0]
     cfg["run_dir"] = cv_dir
+    cfg["train_basin_file"]=Path(f"/home/bernhard/git/Master-Thesis/runs/correlation_reduction/cross_validation/cross_validation_seed_19970204/{i}/basins_train.txt")
     cfg["val_basin_file"] = Path(
         f"/home/bernhard/git/Master-Thesis/runs/correlation_reduction/cross_validation/cross_validation_seed_19970204/{i}/basins_val.txt"
     )
