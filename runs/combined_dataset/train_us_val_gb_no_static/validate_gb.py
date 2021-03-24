764735170dfb23ced0b29f3dc82910c595a6d347
bernharl <bernhardnorneslotsberg@gmail.com>
2021-03-24 13:42:01 +0100

    Fix gb val

--- runs/combined_dataset/train_us_val_gb_no_static/validate_gb.py
+++ runs/combined_dataset/train_us_val_gb_no_static/validate_gb.py
@@ -4,17 +4,17 @@ from camelsml import evaluate, load_config
 
 cfg = load_config("run_config.txt")
 cfg["val_basin_file"] = Path(
-    f"gb_split/split_seed_19970204/basins_validation.txt"
+    f"../train_us_val_gb/gb_split/split_seed_19970204/basins_validation.txt"
 )
-for k in range(2,5):
-    runs = list(Path(f"/work/bernharl/train_us_val_gb/{k}").glob("*"))
+for k in range(5):
+    runs = list(Path(f"{k}").glob("*"))
     print(runs)
     if len(runs) != 1:
         raise RuntimeError(f"Amount of runs per cross val should be 1, not {len(runs)}")
     cfg["run_dir"] = runs[0]
     cfg["train_basin_file"] = Path(
-        f"cv/cross_validation_seed_19970204/{k}/basins_train.txt"
+        f"../train_us_val_gb/cv/cross_validation_seed_19970204/{k}/basins_train.txt"
     )
-    cfg["eval_dir"] = Path(f"/work/bernharl/train_us_val_gb/val_gb/{k}")
+    cfg["eval_dir"] = Path(f"val_gb/{k}")
     for i in range(1, cfg["epochs"] + 1):
         evaluate(cfg, split="val", epoch=i)
