3dac2c1f631cb46b8b371aa2c0d1865d7a8e5fc1
Bernhard Nornes Lotsberg <bernharl@g001.cm.cluster>
2021-04-07 14:28:37 +0200

    Add US->GB fixed

---
+++ runs/combined_dataset/train_us_val_gb/validate_gb.py
@@ -0,0 +1,26 @@
+from time import sleep
+from pathlib import Path
+
+from camelsml import evaluate, load_config
+
+
+cfg = load_config("run_config.txt", device="cuda:0")
+cfg["val_basin_file"] = Path(
+    f"gb_split/split_seed_19970204/basins_validation.txt"
+)
+for k in range(5):
+    runs = list(Path(f"/work/bernharl/train_us_val_gb/{k}").glob("*"))
+    print(runs)
+    if len(runs) != 1:
+        raise RuntimeError(f"Amount of runs per cross val should be 1, not {len(runs)}")
+    cfg["run_dir"] = runs[0]
+    cfg["train_basin_file"] = Path(
+        f"cv/cross_validation_seed_19970204/{k}/basins_train.txt"
+    )
+    cfg["eval_dir"] = Path(f"/work/bernharl/train_us_val_gb/val_gb/{k}")
+    if k == 0:
+        start = 25
+    else:
+        start = 0 
+    for i in range(start, cfg["epochs"] + 1):
+        evaluate(cfg, split="val", epoch=i)
