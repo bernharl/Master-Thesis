a9ea39e2bbc96fa5acd92292692cab900a085aaa
bernhard <b.n.lotsberg@fys.uio.no>
2021-04-10 16:55:09 +0200

    Add new run with improved attr selection

---
+++ runs/combined_dataset/train_us_val_gb_remove_unrepresented/run.py
@@ -0,0 +1,18 @@
+from pathlib import Path
+
+from camelsml import load_config, train
+
+cfg = load_config(cfg_file="run_config.txt", device="cuda:0", num_workers=60)
+print(cfg["attribute_selection_file"])
+cfg["test_basin_file"] = Path(
+    "/home/bernharl/git/Master-Thesis/runs/combined_dataset/train_us_val_gb/cv/cross_validation_seed_19970204/basins_test.txt"
+)
+for i in range(0, 5):
+    cfg["run_dir"] = Path(f"{i}")
+    cfg["train_basin_file"] = Path(
+        f"/home/bernhard/git/Master-Thesis/runs/combined_dataset/train_us_val_gb/cv/cross_validation_seed_19970204/{i}/basins_train.txt"
+    )
+    cfg["val_basin_file"] = Path(
+        f"/home/bernhard/git/Master-Thesis/runs/combined_dataset/train_us_val_gb/cv/cross_validation_seed_19970204/{i}/basins_val.txt"
+    )
+    train(cfg)
